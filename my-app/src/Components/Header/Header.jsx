cf4d51c85c0164dbd63a35983a433ea7a504b107
Glo_study <3809164@gmail.com>
2022-05-31 16:15:06 +0300

    Header

---
+++ my-app/src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import header from "../Header/Header.module.css"
+
+const Header = () => {
+    return(
+        <div className="header">
+            <h2 className="title">Header</h2>
+        </div>
+    )
+}
+
+export default Header
